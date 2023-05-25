72c9ac01953ab6bbedbc9835c947653c752affd6
Rich Willock <richard.willock@iqvia.com>
2023-05-25 14:48:25 +0100

    Clear up changes Macros and packages code

--- jaffle_shop/target/run/jaffle_shop/models/example/my_second_dbt_model.sql
+++
@@ -1,9 +0,0 @@
-create or replace view `test_rw`.`my_second_dbt_model`
-  
-  
-  as
-    -- Use the `ref` function to select from other models
-
-select *
-from `test_rw`.`my_first_dbt_model`
-where id = 1
